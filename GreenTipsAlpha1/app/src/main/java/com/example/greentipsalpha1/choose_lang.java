e1a39adbe9dd9afeafb90fcf1c29743d5f723dd5
Endrit Karpuzi <endrit.karpuzi@gmail.com>
2020-09-23 22:53:08 +0200

    Early UI
    Early version of UI

---
+++ GreenTipsAlpha1/app/src/main/java/com/example/greentipsalpha1/choose_lang.java
@@ -0,0 +1,14 @@
+package com.example.greentipsalpha1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class choose_lang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_lang);
+    }
+}
---
+++ GreenTipsAlpha1/app/src/main/java/com/example/greentipsalpha1/welcome.java
@@ -0,0 +1,28 @@
+package com.example.greentipsalpha1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+
+    public void chooseLangScreen(View v){
+        startActivity(new Intent(welcome.this, choose_lang.class));
+    }
+
+    public void aboutScreen(View v){
+        startActivity(new Intent(welcome.this, about.class));
+    }
+
+    public void helpScreen(View v){
+        startActivity(new Intent(welcome.this, help.class));
+    }
+}
