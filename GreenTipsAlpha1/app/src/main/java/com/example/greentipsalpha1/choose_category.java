3c1d91c2f0afe6c2ebba242ead6149cb036a0e02
Endrit Karpuzi <endrit.karpuzi@gmail.com>
2020-10-04 19:58:47 +0200

    Early UI

--- GreenTipsAlpha1/app/src/main/java/com/example/greentipsalpha1/choose_category.java
+++ GreenTipsAlpha1/app/src/main/java/com/example/greentipsalpha1/choose_category.java
@@ -2,17 +2,17 @@ package com.example.greentipsalpha1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class choose_lang extends AppCompatActivity {
+public class choose_category extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_lang);
+        setContentView(R.layout.activity_choose_category);
     }
 
-    public void gotransport(View view) {
-    }
+    public void gotransport (View v){ startActivity(new Intent(choose_category.this, transportTips.class)); }
 }
--- GreenTipsAlpha1/app/src/main/java/com/example/greentipsalpha1/transportTips.java
+++ GreenTipsAlpha1/app/src/main/java/com/example/greentipsalpha1/transportTips.java
@@ -3,16 +3,12 @@ package com.example.greentipsalpha1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class choose_lang extends AppCompatActivity {
+public class transportTips extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_lang);
-    }
-
-    public void gotransport(View view) {
+        setContentView(R.layout.activity_transport_tips);
     }
 }
