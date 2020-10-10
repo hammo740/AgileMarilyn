b728e2fcb7f5c14e96c510ca5e2e6410036862e8
Cezary Januszek <cezary.januszek@epfl.ch>
2020-10-10 20:00:45 +0200

    BaseActivity layout

--- GreenTipsAlpha1/app/src/main/java/com/example/greentipsalpha1/BaseActivity.java
+++ GreenTipsAlpha1/app/src/main/java/com/example/greentipsalpha1/BaseActivity.java
@@ -3,6 +3,7 @@ package com.example.greentipsalpha1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,7 +13,20 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.base_activity);
+
+        /*ImageButton buttonLanguage = (ImageButton) findViewById(R.id.languageButton);
+        buttonLanguage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotToLanguageScreen();
+            }
+        });*/
     }
+
+    /*public void gotToLanguageScreen(){
+        Intent intent = new Intent(BaseActivity.this, choose_lang.class);
+        startActivity(intent);
+    }*/
 
     public void chooseLangScreen(View v){ startActivity(new Intent(this, choose_lang.class)); }
 
--- GreenTipsAlpha1/app/src/main/java/com/example/greentipsalpha1/welcome.java
+++ GreenTipsAlpha1/app/src/main/java/com/example/greentipsalpha1/welcome.java
@@ -4,16 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
-public class welcome extends BaseActivity {
+public class welcome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        /*ImageButton buttonLanguage = (ImageButton) findViewById(R.id.languageButton);
+        buttonLanguage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotToLanguageScreen();
+            }
+        });*/
     }
 
-    @Override
+    /*public void gotToLanguageScreen(){
+        Intent intent = new Intent(welcome.this, choose_lang.class);
+        startActivity(intent);
+    }*/
+
     public void chooseLangScreen(View v){ startActivity(new Intent(welcome.this, choose_lang.class)); }
 
     public void aboutScreen(View v){
