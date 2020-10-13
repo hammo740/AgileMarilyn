82ec446338380c29e380e40a0baac896726a8eb2
Cezary Januszek <cezary.januszek@epfl.ch>
2020-10-13 22:47:47 +0200

    Bug splashScreen resolved

--- GreenTipsAlpha1/app/src/main/java/com/example/greentipsalpha1/splashscreen.java
+++ GreenTipsAlpha1/app/src/main/java/com/example/greentipsalpha1/splashscreen.java
@@ -2,6 +2,7 @@ package com.example.greentipsalpha1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,21 +11,22 @@ import java.util.TimerTask;
 
 public class splashscreen extends AppCompatActivity {
 
-    Timer timer;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
 
-        timer = new Timer();
-        timer.schedule(new TimerTask() {
+        handler = new Handler();
+        handler.postDelayed(new Runnable(){
             @Override
-            public void run() {
+            public void run(){
                 Intent intent = new Intent(splashscreen.this, welcome.class);
                 startActivity(intent);
-                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                finish();
             }
         }, 4000);
+
     }
 }
