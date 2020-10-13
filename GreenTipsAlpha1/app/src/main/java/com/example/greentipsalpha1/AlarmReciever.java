260d47da97e66c81ba238fa95b924a3ea26e69fd
Alexis <alexis@student.chalmers.se>
2020-10-13 19:58:50 +0200

    Updated files

---
+++ GreenTipsAlpha1/app/src/main/java/com/example/greentipsalpha1/AlarmReciever.java
@@ -0,0 +1,28 @@
+package com.example.greentipsalpha1;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReciever extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        int notificationId = intent.getIntExtra("notificationId", 0);
+        String msg = intent.getStringExtra("messegae");
+        Intent mainIntent = new Intent(context, welcome.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, mainIntent, 0);
+        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification.Builder builder = new Notification.Builder(context);
+        builder.setSmallIcon(R.drawable.ic_baseline_notification_active_24)
+                .setContentTitle("It's Time")
+                .setContentText(msg)
+                .setWhen(System.currentTimeMillis())
+                .setAutoCancel(true)
+                .setContentIntent(contentIntent)
+                .setPriority(Notification.PRIORITY_MAX)
+                .setDefaults(Notification.DEFAULT_ALL);
+        notificationManager.notify(notificationId,builder.build());
+    }
+}
---
+++ GreenTipsAlpha1/app/src/main/java/com/example/greentipsalpha1/ScheduleActivity.java
@@ -0,0 +1,66 @@
+package com.example.greentipsalpha1;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+
+public class ScheduleActivity extends AppCompatActivity {
+
+    private int NotificationId = 1;
+    EditText editText;
+    TimePicker timePicker;
+    Button set, cancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+
+        editText = findViewById(R.id.et);
+        timePicker = findViewById(R.id.time);
+        set = findViewById(R.id.btnSet);
+        cancel = findViewById(R.id.btnCancel);
+
+        final AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+        set.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (ScheduleActivity.this, AlarmReciever.class);
+                intent.putExtra ("notificationId", NotificationId);
+                intent.putExtra ("message", editText.getText().toString());
+                final PendingIntent pendingIntent = PendingIntent.getBroadcast(ScheduleActivity.this, 0,intent, PendingIntent.FLAG_CANCEL_CURRENT);
+                int Hour = timePicker.getCurrentHour();
+                int Minute = timePicker.getCurrentMinute();
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(Calendar.HOUR_OF_DAY,Hour);
+                calendar.set(Calendar.MINUTE,Minute);
+                calendar.set(Calendar.SECOND,0);
+
+                long alarmStartTime = calendar.getTimeInMillis();
+                long alarmSartTime = 0;
+                alarmManager.set(AlarmManager.RTC_WAKEUP,alarmSartTime,pendingIntent);
+
+                Toast.makeText(ScheduleActivity.this,"Done",Toast.LENGTH_SHORT).show();
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (ScheduleActivity.this, AlarmReciever.class);
+                final PendingIntent pendingIntent = PendingIntent.getBroadcast(ScheduleActivity.this, 0,intent, PendingIntent.FLAG_CANCEL_CURRENT);
+                alarmManager.cancel(pendingIntent);
+                Toast.makeText(ScheduleActivity.this,"Cancel",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
